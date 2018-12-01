763a2c44559792e1d0d53a6bbb648ca722b67ac8
IndonesianDarkness <45517715+IndonesianDarkness@users.noreply.github.com>
2018-12-02 00:00:15 +0700

    Create Tollsv1.sh

---
+++ Tollsv1.sh
@@ -0,0 +1,486 @@
+clear
+#!/system/xbin/bash
+#ngapain lu bangsat
+#mau record?
+#apa faedah nya?#biar di bilang mastah?
+#kok gua ngukuk ya
+#lu ngerecord gak bakal ngubah lu jadi mastah goblok
+#klo gak suka gelud aja kyta!1!1
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+#colors
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+echo $cyan
+figlet  "tolls installer v1"
+echo $cyan"====================================================================="
+echo $cyan" [*]$cyan############################################################$cyan[*]"
+echo $cyan" |*|  $yellow        Author   : ./Kagamine404                          $cyan|*|"
+echo $cyan" |*|  $yellow        Team     : Indonesian Darkness                  $cyan  |*|"
+echo $cyan" |*|  $yellow        Contact  : 0895378276565                       $cyan   |*|"
+echo $cyan" [*]############################################################[*]"
+echo ""
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[1]$white Install santet online                                   $cyan  [+]"
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[2]$white Install RED_HAWK                                         $cyan [+] "
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[3]$white Install MBF buat hek efbi                                $cyan [+]"
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[4]$white Install LITESCRIPT                                       $cyan [+]"
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[5]$white Install BinGoo                                           $cyan [+]"
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[6]$white Install LITESPAM                                         $cyan [+]"
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[7]$white Install Websploit                                        $cyan [+]"
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[8]$white Install Findip                                           $cyan [+] "
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[9]$white Inatall Admin penal                                      $cyan [+]"
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[10]$white Install Lazymux                                         $cyan [+]"
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[11]$white Install BlackHydra                                      $cyan [+]"
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[12]$white Install Tolls Cyb3r_Gh0st v3                            $cyan [+] "
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[13]$white Install Tolls BAJINGAN v6                               $cyan [+]"
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[14]$white Install Tolls Clown Cyber Army                          $cyan [+]"
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[15]$white Install NetHunter                                       $cyan [+]   "
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[16]$white Install SEMUUT buat tembak kouta                        $cyan [+] "
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[17]$white Install WIFIHACKER    (Root                             $cyan [+]"
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[18]$white Install Hunner Framework                                $cyan [+]"
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[19]$white Install D-TECT                                          $cyan [+]"
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[20]$white Install Hash Buster                                     $cyan [+]"
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[21]$white Install Tolls Mr.Cakil                                  $cyan [+]"
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[22]$white Install Tolls xNot_Found v2                             $cyan [+]"
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[23]$white Install AEX                                             $cyan [+]"
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[24]$white Install Tolls Crewbot                                   $cyan [+]"
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[25]$white Install Torshammer                                      $cyan [+]"
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[26]$white Install Tolls daijobu                                   $cyan [+]"
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[27]$white Install RECONDOG                                        $cyan [+]"
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[28]$white Install SQLMAP                                          $cyan [+]"
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[29]$white Install HAKKU                                           $cyan {+}"
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[30]$white Install CreaterVirus                                    $cyan [+]"
+echo $yellow"[+]===========================================================[+]"
+echo $cyan"[00]$white keluar/exit                                             $cyan [+]"
+echo $yellow"[+]===========================================================[+]"
+echo $green
+read -p "root@./Kagamine404~# " pilihan 
+
+if [ $pilihan = "00" ] || [ $pilihan = "00" ]
+then
+echo $cyan
+figlet "keluar"
+echo $white "Terimakasi telah menggunakan tols kami semoga berfaedah :)"
+fi
+
+if [ $pilihan = "01" ] || [ $pilihan = "1" ]
+then
+clear
+figlet "Menginstall"
+apt update && apt upgrade
+apt install python2
+pkg install git
+git clone https://github.com/Gameye98/santet-online
+cd santet-online
+python2 santet.py
+fi
+
+if [ $pilihan = "02" ] || [ $pilihan = "2" ]
+then
+clear
+figlet "Menginstall"
+apt update&&apt upgrade
+apt install php
+apt install git
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+chmod +x rhawk.php
+php rhawk.php
+fi
+
+if [ $pilihan = "03" ] || [ $pilihan = "3" ]
+then
+clear
+figlet "Menginstall"
+pkg update && upgrade
+pkg install python2
+pkg install git
+pip2 install mechanize
+git clone https://github.com/hnov7/mbf
+cd mbf
+python2 mbf.py
+fi
+
+if [ $pilihan = "04" ] || [ $pilihan = "4" ]
+then
+clear
+figlet "Menginstall"
+pkg install python2 git
+git clone https://github.com/4L13199/LITESCRIPT
+cd LITESCRIPT
+python2 LITESCRIPT.py
+fi
+
+if [ $pilihan = "05" ] || [ $pilihan = "5" ]
+then
+clear
+figlet "Menginstall"
+apt update &&amp; upgrade -y
+apt install curl
+apt install grep
+apt install linx
+apt install git
+git clone https://github.com/Hood3dRob1n/BinGoo.git
+cd BinGoo
+termux-fix-shebang bingoo
+chmod +x bingoo
+bash bingoo
+fi
+
+if [ $pilihan = "06" ] || [ $pilihan = "6" ]
+then
+clear
+figlet "Menginstall"
+apt update
+apt upgrade
+pkg install php
+pkg install git
+pkg install toilet
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $pilihan = "07" ] || [ $pilihan = "7" ]
+then
+clear
+figlet "menginstall"
+apt update && upgrade -y
+apt install python2
+pip2 install scapy
+pkg install git
+git clone https://github.com/The404Hacking/websploit.git
+cd websploit
+chmod 777 *.py
+python2 websploit.py
+fi
+
+if [ $pilihan = "08" ] || [ $pilihan = "8" ]
+then
+clear
+figlet "menginstall"
+apt install git
+git clone https://github.com/kereh/Findip
+cd Findip
+python2 findip.py
+fi
+
+if [ $pilihan = "09" ] || [ $pilihan = "9" ]
+then
+clear
+figlet "menginstall"
+pkg install git
+git clone https://github.com/Techzindia/admin_penal/
+cd admin_penal
+python2 main.py
+fi
+
+if [ $pilihan = "10" ] || [ $pilihan = "10" ]
+then
+clear
+figlet "menginstall"
+apt update
+apt install git
+git clone https://github.com/Gameye98/Lazymux
+cd Lazymux
+python2 Lazymux.py
+fi
+
+if [ $pilihan = "11" ] || [ $pilihan = "11" ]
+then
+clear
+figlet "menginstall"
+fi
+
+if [ $pilihan = "12" ] || [ $pilihan = "12" ]
+then
+clear
+figlet "menginstall"
+pkg update && pkg upgrade
+pkg install figlet
+pkg install php
+pkg install python python2 vim figlet curl
+pip2 install lolcat
+pkg install git
+git clone
+https://github.com/cyberghost69/bima-bagus-jabang-v3
+cd bima-bagus-jabang-v3
+sh bima-bagus-jabang-v3.sh
+echo $cyan "USER: bima-jabang"
+echo $cyan "PASWD: jabang"
+fi
+
+if [ $pilihan = "13" ] || [ $pilihan = "13" ]
+then
+clear
+figlet "menginstall"
+apt update apt upgrade
+apt install git
+apt install php
+apt install figlet
+apt install toilet
+apt install python2
+pip2 install requests
+pip2 install termcolor
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6.git
+cd BAJINGANv6
+sh BAJINGAN.sh
+echo $cyan "USER: BAJINGAN"
+echo $cyan "PASWD: Gans"
+fi
+
+if [ $pilihan = "14" ] || [ $pilihan = "14" ]
+then
+clear
+figlet "menginstall"
+pkg install git 
+git clone https://github.com/killerclown12/CCA
+cd CCA
+sh killerv1.sh
+fi
+
+if [ $pilihan = "15" ] || [ $pilihan = "15" ]
+then
+clear
+figlet "menginstall"
+apt update
+apt install git
+git clone https://github.com/Hax4us/Nethunter-In-Termux.git
+cd Nethunter
+Is
+chmod +x kalinethunter
+./kalinethunter
+./startkali.sh
+fi
+
+if [ $pilihan = "16" ] || [ $pilihan = "16" ]
+then
+clear
+figlet "menginstall"
+pkg install python
+pkg install git
+git clone https://github.com/kumpul4semut/semut.git
+cd semut
+python dor.py
+fi
+
+if [ $pilihan = "17" ] || [ $pilihan = "17" ]
+then
+clear
+figlet "menginstall"
+apt upgrade
+pkg install git
+git clone https://github.com/esc0rtd3w/wifi-hacker
+cd wifi-hacker
+chmod +x wifi-hacker.sh
+./wifi-hacker.sh
+fi
+
+if [ $pilihan = "18" ] || [ $pilihan = "18" ]
+then
+clear
+figlet "menginstall"
+apt update
+apt install python
+apt install git -y
+git clone https://github.com/b3-v3r/Hunner
+cd Hunner 
+chmod 777 hunner.py
+python hunner.py
+fi
+
+if [ $pilihan = "19" ] || [ $pilihan = "19" ]
+then
+clear
+figlet "menginstall"
+apt update
+apt install git
+git clone https://github.com/shawarkhanethicalhacker/D-TECT
+cd D-TECT
+apt install python2
+chmod +x d-tect.py
+python2 d-tect.py
+fi
+
+if [ $pilihan = "20" ] || [ $pilihan = "20" ]
+then
+clear
+figlet "menginstall"
+apt update
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/UltimateHackers/Hash-Buster
+cd Hash-Buster
+python2 hash.py 
+fi
+
+if [ $pilihan = "21" ] || [ $pilihan = "21" ]
+then
+clear
+figlet "menginstall"
+apt update && apt upgrade
+apt install git
+git clone https://github.com/mrcakil/Mrcakil.git 
+cd Mrcakil 
+chmod +x tools 
+./tools
+fi
+
+if [ $pilihan = "22" ] || [ $pilihan = "22" ]
+then
+clear
+figlet "menginstall"
+apt update && apt upgrade
+pkg install python python2 vim figlet curl gem git toilet
+pkg install php
+gem install lolcat
+git clone https://github.com/hatakecnk/xNot_Found
+cd xNot_Found
+chmod +x xNot_Found.sh
+sh xNot_Found.sh
+fi
+
+if [ $pilihan = "23" ] || [ $pilihan = "23" ]
+then
+clear
+figlet "menginstall"
+apt update
+pkg install git
+git clone https://github.com/Rajkumrdusad/Tool-X.git
+cd Tool-X
+chmod +x install.aex
+sh install.aex kalo ngga bisa pake
+./install.aex
+fi
+
+if [ $pilihan = "24" ] || [ $pilihan = "24" ]
+then
+clear
+figlet "menginstall"
+apt update
+apt upgrade
+pkg install
+git clone https://github.com/Xeit666h05t/CrewBot
+ls
+cd CrewBot
+python2 CrewBot.py
+fi
+
+if [ $pilihan = "25" ] || [ $pilihan = "25" ]
+then
+clear
+figlet "menginstall"
+pkg update
+pkg install git
+apt install tor
+pkg install python2
+git clone https://github.com/dotfighter/torshammer.git
+ls 
+cd torshammer
+python2 torshammer.py
+fi
+
+if [ $pilihan = "26" ] || [ $pilihan = "26" ]
+then
+clear
+figlet "menginstall"
+apt upgrade && apt update 
+apt install php 
+apt install git 
+git clone https://github.com/alintamvanz/diejoubu 
+cd diejoubu 
+cd v1.2 
+php diejoubu.php
+fi
+
+if [ $pilihan = "27" ] || [ $pilihan = "27" ]
+then
+clear
+figlet "menginstall"
+apt update
+apt install python python2
+apt install git
+git clone https://github.com/UltimateHackers/ReconDog
+cd ReconDog
+chmod +x dog.py
+Python2 dog.py
+fi
+
+if [ $pilihan = "28" ] || [ $pilihan = "28" ]
+then
+clear
+figlet "menginstall"
+apt update
+apt install python
+apt install python2
+apt install git
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+Python2 sqlmap.py
+fi
+
+if [ $pilihan = "29" ] || [ $pilihan = "29" ]
+then
+clear
+figlet "menginstall"
+apt install python
+apt install git
+mkdir vasu
+git clone https://github.com/4shadoww/hakkuframework
+cd hakkuframework
+chmod +x hakku
+python hakku
+fi
+
+if [ $pilihan = "30" ] || [ $pilihan = "30" ]
+then
+clear
+figlet "menginstall"
+cd /storage/emulated/0
+apt update && apt upgrade
+apt install python2
+gem install lolcat
+apt install figlet
+apt install git
+pip2 install mechanize
+git clone https://github.com/viruz09/CreaterVirus.git
+cd CreaterVirus
+ls
+python2 creater.py
+fi
